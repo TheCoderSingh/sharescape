563017d13255c5a34f7105544d16090e21f5d1d0
Jaskaran Singh <thecodesingh@gmail.com>
2023-11-22 02:51:26 -0800

    Change signup component location, Add JSDOC

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,21 @@
+"use client";
+
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+/**
+ * Signup Page
+ * @returns {JSX.Element} The Signup component JSX
+ */
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <Typography>
+        <Title>Signup</Title>
+      </Typography>
+    </div>
+  );
 };
 
 export default Signup;
