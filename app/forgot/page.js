75314fb68d718808369675ee470d9a7655a5c075
Jaskaran Singh <thecodesingh@gmail.com>
2023-11-23 19:09:49 -0800

    Implement forgot page

--- app/forgot/page.js
+++ app/forgot/page.js
@@ -1,5 +1,44 @@
+"use client";
+import { Button, Flex, Form, Input, Typography } from "antd";
+import Link from "next/link";
+
+import "../globals.css";
+
+const { Text, Title } = Typography;
+
+/**
+ * Forgot Page
+ * @returns {JSX.Element} The Forgot component JSX
+ */
 const Forgot = () => {
-  return <div>Forgot</div>;
+  return (
+    <Flex className="forgot" vertical justify="center">
+      <Typography>
+        <Title level={4}>
+          Let&apos;s help you recover your <span>Sharescape</span> account
+        </Title>
+      </Typography>
+
+      <Form name="forgot" className="forgot-form">
+        <Form.Item name="username">
+          <Input placeholder="What's your Email?" />
+        </Form.Item>
+        <Button>Next</Button>
+      </Form>
+
+      <Flex className="bottom-links" justify="center" gap={5}>
+        <Typography>
+          <Text>
+            Want to
+            <Link href="/login"> Login </Link>
+            or
+            <Link href="/signup"> Signup </Link>
+            instead?
+          </Text>
+        </Typography>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Forgot;
