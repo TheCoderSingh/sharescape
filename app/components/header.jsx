b38b91ac8666dca8c07ce9647b0ec6983dcc98d3
Jaskaran Singh <thecodesingh@gmail.com>
2023-11-24 00:44:24 -0800

    Implement header

---
+++ app/components/header.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { Flex, Typography } from "antd";
+import Link from "next/link";
+const { Title } = Typography;
+
+import "../globals.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Flex justify="space-between">
+        <Typography>
+          <Title level={2}>Sharescape</Title>
+        </Typography>
+        <Flex gap={14} align="center">
+          <div>
+            <Link href="/buddies">Find Buddies</Link>
+          </div>
+          <div>
+            <Link href="/profile">Profile</Link>
+          </div>
+          <div>
+            <Link href="/messages">Messages</Link>
+          </div>
+          <div>
+            <Link href="/settings">Settings</Link>
+          </div>
+          <div>
+            <Link href="/logout">Logout</Link>
+          </div>
+        </Flex>
+      </Flex>
+    </div>
+  );
+};
+
+export default Header;
